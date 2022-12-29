fd350e299876d8a766bbf78aee27c014c4854eb6
sasaki_taku <ssktaku6172@gmail.com>
2022-12-29 13:05:08 +0900

    [Fix] アイコンフォントの型生成に不具合があったため修正

--- src/styles/foundation/base/_iconfont-type.scss.d.ts
+++ src/styles/foundation/base/_iconfont-type.scss.d.ts
@@ -1,15 +1,23 @@
-export const file: string;
-export const file2: string;
-export const home: string;
-export const medal: string;
-export const snsFacebook: string;
-export const snsFacebook2: string;
-export const snsInstagram: string;
-export const snsLine: string;
-export const snsLine2: string;
-export const snsLine3: string;
-export const snsLine4: string;
-export const snsTwitter: string;
-export const upload: string;
-export const upload2: string;
-export const userCircle: string;
+export type Styles = {
+  file: string;
+  file2: string;
+  home: string;
+  medal: string;
+  sns_facebook: string;
+  sns_facebook2: string;
+  sns_instagram: string;
+  sns_line: string;
+  sns_line2: string;
+  sns_line3: string;
+  sns_line4: string;
+  sns_twitter: string;
+  upload: string;
+  upload2: string;
+  'user-circle': string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
