cfd6d219d3679fbc42d4aab3c250c4216ea40d4e
sasaki_taku <ssktaku6172@gmail.com>
2022-12-29 18:27:41 +0900

    [WIP] 不要ファイル削除など細々やった

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
+  // base: '/docs',
   integrations: [react()],
   vite: {
     resolve: {
--- src/components/Title.tsx
+++
@@ -1,8 +0,0 @@
-type TitleProps = {
-  children: React.ReactNode;
-};
-
-const Title: React.FC<TitleProps> = ({ children }) => {
-  return <h2>{children}</h2>;
-};
-export default Title;
