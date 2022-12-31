a0a7a223bbdf39a63cec16ae03fbb487ac66ffbb
sasaki_taku <ssktaku6172@gmail.com>
2022-12-31 12:24:28 +0900

    [Add] ビルド時にgulpで画像を圧縮するタスクを追加

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,18 @@
 const gulp = require("gulp");
 const rename = require('gulp-rename');
 
-
 //アイコンフォント作成用
 const iconfont = require('gulp-iconfont');
 const consolidate = require('gulp-consolidate');
 const iconFontOutputDirectory = "public/";
 const runTimestamp = Math.round(Date.now() / 1000);
 const fontName = 'iconfont'; // シンボルフォント名
+
+//画像圧縮
+const imagemin = require('gulp-imagemin'); //v7.1.0を使うこと
+const mozjpeg = require('imagemin-mozjpeg'); //v9.0.0を使うこと
+const pngquant = require('imagemin-pngquant');
+const changed = require('gulp-changed');
 
 
 //アイコンフォント ジェネレーター
@@ -58,7 +63,25 @@ const task_iconfont = () => {
     .pipe(gulp.dest(iconFontOutputDirectory + 'fonts/'));
 };
 
-
+//画像を圧縮して移動
+const task_imagemin = () => {
+  return gulp
+    .src('public/images/**')
+    .pipe(changed('dist/assets/img'))
+    .pipe(
+      imagemin([
+        pngquant({
+          quality: [.60, .70], // 画質
+          speed: 1 // スピード
+        }),
+        mozjpeg({ quality: 65 }), // 画質
+        imagemin.svgo(),
+        imagemin.optipng(),
+        imagemin.gifsicle({ optimizationLevel: 3 }) // 圧縮率
+      ])
+    )
+    .pipe(gulp.dest('dist/images'));
+};
 
 //監視タスク
 const task_watch = () => {
@@ -74,4 +97,5 @@ const task_watch = () => {
 //デフォルト実行タスク
 exports.default = gulp.series(task_watch);
 
-// exports.build = gulp.series(execute_sass_end_iconfont, task_imagemin, task_filecopy);
+//ビルド時の実行タスク
+exports.build = gulp.series(task_imagemin);
